3b49ebdbf433c0feaa37f319f4747ec0d3c59ff3
Adrien 42 <adr.bidaux@gmail.com>
2025-03-18 16:17:42 +0100

    notwell

--- parsing.c
+++ parsing.c
@@ -6,7 +6,7 @@
 /*   By: abidaux <abidaux@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/17 21:09:16 by abidaux           #+#    #+#             */
-/*   Updated: 2025/03/17 21:28:28 by abidaux          ###   ########.fr       */
+/*   Updated: 2025/03/18 16:15:13 by abidaux          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,22 +14,15 @@
 
 void	parsing(char *input)
 {
+	//
+}
+
+t_token	*init_token(char *str, int type)
+{
 	t_token	*token;
-	int		i;
 
 	token = malloc(sizeof(t_token));
-	i = -1;
-	while (input[++i])
-	{
-		if (input[i] == '<')
-			token->type = 1;
-		else if (input[i] == '>')
-			token->type = 3;
-		else if (input[i] == '|')
-			token->type = 5;
-		else if (input[i] == ' ')
-			token->next = malloc(sizeof(t_token));
-		else
-			printf("CMD\n");
-	}
+	if (!token)
+		return (NULL);
+	
 }
