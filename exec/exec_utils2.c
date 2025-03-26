cbc4bfc27ee20b83f270882d80977662d5f72fac
Adrien 42 <adr.bidaux@gmail.com>
2025-03-26 17:25:06 +0100

    add in & out redirection in exec

--- exec/exec_utils2.c
+++ exec/exec_utils2.c
@@ -6,12 +6,19 @@
 /*   By: abidaux <abidaux@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/26 16:47:41 by abidaux           #+#    #+#             */
-/*   Updated: 2025/03/26 16:50:41 by abidaux          ###   ########.fr       */
+/*   Updated: 2025/03/26 17:24:27 by abidaux          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../minishell.h"
 
+/**
+ * Vérifie si le chemin spécifié correspond à un répertoire.
+ *
+ * @param path Le chemin à vérifier
+ * @return 1 si ce n'est pas un répertoire ou si stat échoue, 0 si c'est un répertoire
+ *         (affiche également un message d'erreur dans ce cas)
+ */
 static int	check_if_directory(char *path)
 {
 	struct stat	st;
@@ -22,4 +29,88 @@ static int	check_if_directory(char *path)
 		return (0);
 	}
 	return (1);
+}
+
+/**
+ * Gère une redirection d'entrée (< file).
+ * Vérifie que la cible n'est pas un répertoire, ouvre le fichier en lecture
+ * et redirige l'entrée standard vers ce fichier.
+ *
+ * @param redir Structure contenant les informations de redirection
+ * @param state État global du shell
+ * @return 0 en cas de succès, -1 en cas d'erreur
+ */
+static int	handle_input_redirection(t_redir *redir, t_state *state)
+{
+	int	fd;
+
+	if (!check_if_directory(redir->target))
+		return (-1);
+	fd = open(redir->target, O_RDONLY);
+	if (fd < 0)
+		return (handle_acces_error(redir->target, state));
+	if (dup2(fd, STDIN_FILENO) < 0)
+	{
+		prerror("dup2");
+		close(fd);
+		return (-1);
+	}
+	close (fd);
+	return (0);
+}
+
+/**
+ * Traite toutes les redirections d'entrée d'une commande.
+ * Parcourt le tableau des redirections d'entrée et applique chacune d'elles.
+ *
+ * @param cmd Structure de commande contenant les redirections à traiter
+ * @param state État global du shell
+ * @return 0 en cas de succès, -1 si une des redirections échoue
+ */
+static int	process_input_redirection(t_command *cmd, t_state *state)
+{
+	int	i;
+
+	i = -1;
+	while (++i < cmd->nb_in)
+		if (handle_input_redirection(cmd->in[i], state) == -1)
+			return (-1);
+	return (0);
+}
+
+/**
+ * Traite toutes les redirections de sortie d'une commande.
+ * Parcourt le tableau des redirections de sortie et applique chacune d'elles.
+ *
+ * @param cmd Structure de commande contenant les redirections à traiter
+ * @param state État global du shell
+ * @return 0 en cas de succès, -1 si une des redirections échoue
+ */
+static int	process_output_redirections(t_command *cmd, t_state *state)
+{
+	int	i;
+
+	i = -1;
+	while (++i < cmd->nb_out)
+		if (handle_output_redirection(cmd->out[i], state) == -1)
+			return (-1);
+	return (0);
+}
+
+/**
+ * Gère toutes les redirections (entrée et sortie) d'une commande.
+ * Applique d'abord toutes les redirections d'entrée, puis toutes les redirections de sortie.
+ * Si une redirection échoue, la fonction s'arrête immédiatement.
+ *
+ * @param cmd Structure de commande contenant toutes les redirections à traiter
+ * @param state État global du shell
+ * @return 0 en cas de succès, -1 si une des redirections échoue
+ */
+int	handle_all_redirection(t_command *cmd, t_state *state)
+{
+	if (process_input_redirection(cmd, state) == -1)
+		return (-1);
+	if (process_output_redirections(cmd, state) == -1)
+		return (-1);
+	return (0);
 }
